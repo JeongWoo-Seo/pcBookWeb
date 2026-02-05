423cc8205dfdb4dd8e607281f61e6016b8339f08
JeongWoo-Seo <sjwoo6500@naver.com>
2026-02-05 20:31:27 +0900

    add client list by laptop id

--- server/internal/network/ws/hub.go
+++ server/internal/network/ws/hub.go
@@ -6,38 +6,64 @@ type WSMessage struct {
 }
 
 type Hub struct {
-	Clients    map[*Client]bool
 	Register   chan *Client
 	Unregister chan *Client
 	Dispatch   chan WSMessage
+
+	BroadcastClients map[*Client]bool
+	ClientsByLaptop  map[string]map[*Client]bool
 }
 
 func NewHub() *Hub {
 	return &Hub{
-		Clients:    make(map[*Client]bool),
 		Register:   make(chan *Client),
 		Unregister: make(chan *Client),
 		Dispatch:   make(chan WSMessage, 256),
+
+		BroadcastClients: make(map[*Client]bool),
+		ClientsByLaptop:  make(map[string]map[*Client]bool),
 	}
 }
 
 func (h *Hub) Run() {
 	for {
 		select {
-
 		case c := <-h.Register:
-			h.Clients[c] = true
-
-		case c := <-h.Unregister:
-			if _, ok := h.Clients[c]; ok {
-				delete(h.Clients, c)
-				close(c.send)
+			if c.mode == "broadcast" {
+				h.BroadcastClients[c] = true
+			} else if c.mode == "single" {
+				if h.ClientsByLaptop[c.laptopID] == nil {
+					h.ClientsByLaptop[c.laptopID] = make(map[*Client]bool)
+				}
+				h.ClientsByLaptop[c.laptopID][c] = true
 			}
 
+		case c := <-h.Unregister:
+			delete(h.BroadcastClients, c)
+
+			if m, ok := h.ClientsByLaptop[c.laptopID]; ok {
+				delete(m, c)
+				if len(m) == 0 {
+					delete(h.ClientsByLaptop, c.laptopID)
+				}
+			}
+
+			close(c.send)
+
 		case msg := <-h.Dispatch:
-			for c := range h.Clients {
-				if c.mode == "broadcast" || (c.mode == "single" && c.laptopID == msg.LaptopID) {
-					c.send <- msg.Payload
+			for c := range h.BroadcastClients {
+				select {
+				case c.send <- msg.Payload:
+				default:
+				}
+			}
+
+			if clients, ok := h.ClientsByLaptop[msg.LaptopID]; ok {
+				for c := range clients {
+					select {
+					case c.send <- msg.Payload:
+					default:
+					}
 				}
 			}
 		}
